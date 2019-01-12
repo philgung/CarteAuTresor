4fc02b63ce32cd43628a42e3bcbf82d9cde87c24
Philippe Gung <philippe.gung@youscribe.com>
2019-01-12 08:29:33 +0100

    Init solution

---
+++ CarteAuTresor/CarteAuTresor.Domain/Carte.cs
@@ -0,0 +1,14 @@
+﻿namespace CarteAuTresor.Domain
+{
+    public class Carte
+    {
+        public Carte(FichierDEntree fichierDEntree)
+        {
+            NbCasesEnLargeur = fichierDEntree.NbCasesEnLargeurDeLaCarte;
+            NbCasesEnHauteur = fichierDEntree.NbCasesEnHauteurDeLaCarte;
+        }
+
+        public object NbCasesEnLargeur { get; internal set; }
+        public object NbCasesEnHauteur { get; internal set; }
+    }
+}
---
+++ CarteAuTresor/CarteAuTresor.Domain/FichierDEntree.cs
@@ -0,0 +1,12 @@
+﻿namespace CarteAuTresor.Domain
+{
+    public class FichierDEntree
+    {
+        public FichierDEntree()
+        {
+        }
+
+        public int NbCasesEnLargeurDeLaCarte { get; set; }
+        public int NbCasesEnHauteurDeLaCarte { get; set; }
+    }
+}
